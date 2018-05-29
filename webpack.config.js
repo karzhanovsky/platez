a01c1abb1a326645a98a90d7d8ecb37e4e0f746e
karzhanovsky <alex.karzanowski@gmail.com>
2018-05-29 20:50:24 +0200

    major packages update. Dependencies using newer versions, new loader, scripts, webpack v4, babel

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
