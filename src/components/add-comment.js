a001bbcb3424c9d25b874814abd6ecc2e32e7059
karzhanovsky <alex.karzanowski@gmail.com>
2018-06-05 16:13:54 +0200

    AddComment with an image remodel, image auto-update, image removal, loading gif

--- src/components/add-comment.js
+++ src/components/add-comment.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
-import { db } from '../firebase';
 import firebase from 'firebase';
+import { db } from '../firebase';
+import _ from 'lodash';
 
 class AddComment extends Component {
   constructor(props) {
-    super(props);
+  super(props);
 
-    this.state = {
-      plate: this.props.plate,
-      term: '',
-      image: '',
-      imageUrl: '',
-    }
-
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onImageAdded = this.onImageAdded.bind(this);
+  this.state = {
+    plate: this.props.plate,
+    term: '',
+    imageUrl: '',
   }
+
+  this.onFormSubmit = this.onFormSubmit.bind(this);
+  this.onInputChange = this.onInputChange.bind(this);
+  this.onImageAdded = this.onImageAdded.bind(this);
+  this.removeThumbnail = this.removeThumbnail.bind(this);
+}
 
   onFormSubmit(event) {
     event.preventDefault();
-    if (this.state.image !== '') {
-      this.commentWithImage();
-    } else {
-      this.commentOnly();
-    }
+    db.ref(`/plates/${this.state.plate}/comments`).push({
+      content: this.state.term,
+      author: 'Anonymous',
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      imageUrl: this.state.imageUrl,
+    });
+    this.setState({
+      term: '',
+      imageUrl: '',
+    });
+    let scrolledElement = document.querySelector('.comments');
+    scrolledElement.scrollTop = 0;
   }
 
   onInputChange(event) {
@@ -34,86 +42,65 @@ class AddComment extends Component {
   }
 
   onImageAdded() {
-    let inputNode = document.querySelector("#file-upload");
-    if (inputNode.files.length > 0) {
-      this.setState({
-        image: inputNode.files[0],
+    let fileInputNode = document.querySelector("#file-upload");
+    this.setState({
+      imageUrl: 'src/loading.gif',
+    })
+    let that = this;
+    if (fileInputNode.files.length > 0) {
+      let uploadTask = firebase.storage().ref().child("images/" + new Date()).put(fileInputNode.files[0]);
+      uploadTask.on('state_changed', function(snapshot) {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error){
+        console.log(error);
+      }, function() {
+        uploadTask.snapshot.ref.getDownloadURL()
+        .then(function(downloadURL) {
+          that.setState({
+            imageUrl: downloadURL,
+          })
+        })
       })
     }
   }
 
-  commentWithImage() {
-    let uploadTask = firebase.storage().ref().child("images/" + new Date()).put(this.state.image);
-    var that = this;
-    uploadTask.on('state_changed', function(snapshot) {
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, function(error){
-      console.log(error);
-    }, function() {
-      uploadTask.snapshot.ref.getDownloadURL()
-      .then(function(downloadURL) {
-        that.setState({
-          imageUrl: downloadURL,
-        })
-        db.ref(`/plates/${that.state.plate}/comments`).push({
-          content: that.state.term,
-          author: 'Anonymous',
-          timestamp: firebase.database.ServerValue.TIMESTAMP,
-          imageUrl: that.state.imageUrl,
-        });
-        that.setState({
-          term: '',
-          image: '',
-          imageUrl: '',
-        });
-        let scrolledElement = document.querySelector('.comments');
-        scrolledElement.scrollTop = 0;
-      })
+  removeThumbnail() {
+    this.setState({
+      imageUrl: '',
     })
   }
 
-  commentOnly() {
-    db.ref(`/plates/${this.state.plate}/comments`).push({
-      content: this.state.term,
-      author: 'Anonymous',
-      timestamp: firebase.database.ServerValue.TIMESTAMP,
-    });
-    this.setState({
-      term: '',
-      image: '',
-    });
-    let scrolledElement = document.querySelector('.comments');
-    scrolledElement.scrollTop = 0;
-  }
-
-  render() {
-    return (
-      <div className="add-comment">
-        <form onSubmit={this.onFormSubmit}>
-          <textarea
-          value={this.state.term}
-          onChange={this.onInputChange}
-          placeholder="Dodaj komentarz..."
-          />
-          <label className="file-upload-label">
-            <input
-            type="file"
-            id="file-upload"
-            onChange={this.onImageAdded}
-            accept="image/*" />
-          </label>
-          <button type="submit">+</button>
-        </form>
-      </div>
+render() {
+  return (
+    <div className="add-comment">
+      <form onSubmit={this.onFormSubmit}>
+        {this.state.imageUrl && <div className="comment-image-thumnail" onClick={this.removeThumbnail}>
+          <img src={this.state.imageUrl} />
+        </div>}
+        <textarea
+        value={this.state.term}
+        onChange={this.onInputChange}
+        placeholder="Dodaj komentarz..."
+        />
+        <label className="file-upload-label">
+          <input
+          type="file"
+          id="file-upload"
+          onChange={this.onImageAdded}
+          accept="image/*" />
+        </label>
+        <button type="submit">+</button>
+      </form>
+    </div>
     )
   }
 }
